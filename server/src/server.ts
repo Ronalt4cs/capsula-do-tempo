c14b6c8fe66c016cb780c98c698e07f5d722a592
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-05-16 15:23:11 -0300

    prisma configurado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('Server running on port 3333')
+app.get('/', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
+
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log('Server running on port 3333')
+  })
