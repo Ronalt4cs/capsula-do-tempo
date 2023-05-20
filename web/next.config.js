395cf461567324e005fd6e058ecd2539f44ee2dd
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-05-20 17:43:13 -0300

    adicionar e listar memorias

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'http:/localhost:3333/*'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3333',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
